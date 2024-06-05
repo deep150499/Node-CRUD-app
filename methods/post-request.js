5b6ec697eb43c8147afbcbca036bbd86986bd926
“deep150499” <deepnanavati@icloud.com>
2024-06-05 10:10:06 -0400

    Get and post req

--- methods/post-request.js
+++ methods/post-request.js
@@ -1,3 +1,13 @@
-module.exports = (req,res) => {
-    
+const crypto = require('crypto');
+const requestBodyParser = require('../util/body-parser');
+
+
+module.exports = async (req,res) => {
+    try{
+        let body = await requestBodyParser(req)
+        console.log("Request Body : ",  body)
+    }
+    catch(err){
+
+    }
 }
